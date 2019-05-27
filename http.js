561c38215353c688a9e2271bbe77b0ff2d86f6cd
lin onetwo <linonetwo012@gmail.com>
2019-05-27 10:52:01 +0800

    fix: use fa brand

--- http.js
+++ http.js
@@ -6,7 +6,7 @@ module.exports = () => {
     search: (query) => {
       return new Promise((resolve, reject) => {
         resolve([{
-          icon: 'fa-internet-explorer',
+          icon: 'fa-internet-explorer fab',
           title: query,
           subtitle: 'Open the typed url',
           value: query,
